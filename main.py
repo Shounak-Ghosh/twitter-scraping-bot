622e031ce0ab7d7c6b183a7923760a174779f9b0
Glitch (basic-training-ii) <none>
2020-06-11 21:27:41 +0000

    🎅🏫 Checkpoint ./main.py:2183596/60

--- main.py
+++ main.py
@@ -26,14 +26,13 @@ def generate_message(body, methods=["POST"]):
   try:
     # Get a random tweet from the list of tweets
     single_tweet = random.choice(tweets)
-    bot_answer = moderate(single_tweet)
 
     # Send the answer to the app, to display to the user
-    answer = {"username": twitter_handle, "message": bot_answer}
+    answer = {"username": twitter_handle, "message": moderate(bot_answer)}
     socketio.emit("bot answer", answer)
   except:
     bot_answer = "Sorry, I couldn't process that. Try again please."
-    socketio.emit("error", {"username": twitter_handle, "message": bot_answer})
+    socketio.emit("error", {"username": twitter_handle, "message": moderate(bot_answer)})
 
 if __name__ == "__main__":
     socketio.run(app)
