1401d32568dec7533dd782ea62457eda7006cb3c
Glitch (basic-training-ii) <none>
2021-01-09 00:01:02 +0000

    🏗🎀 Checkpoint ./twitter_scraper_fetcher.py:2190460/68 ./config.py:2190460/34

--- twitter_scraper_fetcher.py
+++ twitter_scraper_fetcher.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from config import TWITTER_URL
 import re
 
 CONTENT_CLASS_NAME = "dir-ltr"
@@ -8,7 +9,6 @@ EMPTY_ITEMS = [None, "", "None", "\n"]
 AGENTS= 'Nokia5310XpressMusic_CMCC/2.0 (10.10) Profile/MIDP-2.1 '\
 'Configuration/CLDC-1.1 UCWEB/2.0 (Java; U; MIDP-2.0; en-US; '\
 'Nokia5310XpressMusic) U2/1.0.0 UCBrowser/9.5.0.449 U2/1.0.0 Mobile'
-TWITTER_URL = "https://twitter.com/"
 
 
 def get_elements(twitter_handle):
